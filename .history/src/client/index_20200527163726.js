e2ca388ed033562d95cc18e680207634ef7c557e
Laura Bobrovacz <bobrovacz.laura@gmail.com>
2020-05-28 11:17:15 +0200

    deleting history

--- .history/src/client/index_20200527163726.js
+++
@@ -1,9 +0,0 @@
-import { submitForm } from './js/app.js'
-import { addTrip } from './js/app.js'
-
-import './styles/app.scss'
-import './styles/header.scss'
-import './styles/footer.scss'
-
-export { submitForm }
-export { addTrip }
